c8e67cafe9a9c95f85d72dfd42f35261d996c239
Samridh Gupta <63790684+SamridhG@users.noreply.github.com>
2021-08-27 10:38:37 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,9 @@
+void deleteAlt(struct Node *head){
+    // Code here
+    Node *temp=head;
+    while( temp!=NULL && temp->next!=NULL)
+    {
+        temp->next=temp->next->next;
+       temp=temp->next;
+    }
+}
